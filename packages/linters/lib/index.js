f25609a8beaba1313a67b2feb3b7491f3e9040e5
contra <yo@contra.io>
2020-08-24 10:46:42 -0700

    fix: deps

--- packages/linters/lib/index.js
+++ packages/linters/lib/index.js
@@ -1,10 +1,8 @@
-const staged = require('./staged')
 const eslint = require('./eslint')
 const stylelint = require('./stylelint')
 const prettier = require('./prettier')
 
 module.exports = {
-  staged,
   eslint,
   stylelint,
   prettier
